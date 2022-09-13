7c9f17f8621b921190695b80a5390714d4efd7f0
Abdihakim Muhumed <64124679+Abdihakim-Muhumed@users.noreply.github.com>
2022-09-13 12:38:07 +0300

    Debugged

--- weatherApp/views.py
+++ weatherApp/views.py
@@ -7,36 +7,34 @@ from datetime import datetime
 # Create your views here.
 
 def index(request):
-    if request.method == 'POST':
-        API_KEY = 'cde0a72800580fe756fdf58fb2984965'
-        city_name = request.POST.get('city')
-        url = f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_KEY}&units=metric'          
-        url = "https://api.openweathermap.org/data/2.5/weather?q=Nairobi&appid=cde0a72800580fe756fdf58fb2984965&units=metric"
-        response = requests.get(url).json()
-        current_time = datetime.now()
-        formatted_time = current_time.strftime("%A, %B %d %Y, %H:%M:%S %p")
-        city_weather_update = {
-            'city': city_name,
-            'description': response['weather'][0]['description'],
-            'icon': response['weather'][0]['icon'],
-            'temperature': 'Temperature: '+ str(response['main']['temp']) + '°C',
-            'country_code': response['sys']['country'],
-            'wind': 'Wind: '+ str(response['wind']['speed']) + 'km/h',
-            'humidity': 'Humidity: '+str(response['main']['humidity']) + '%',
-            'time': formatted_time
-                
-        }
-        
-    else:
-        city_weather_update = {}
-
-    context = {
-        'city_weather_update': city_weather_update
-    }
-        
-    return render(request, 'weatherApp/index.html', context)
-    #try:
+    try:
         # checking for POST method
-    
-    #except:
-    #   return render(request, 'weatherApp/404.html')
+        if request.method == 'POST':
+            API_KEY = 'cde0a72800580fe756fdf58fb2984965'
+            city_name = request.POST.get('city')
+            url = f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_KEY}&units=metric'          
+            response = requests.get(url).json()
+            current_time = datetime.now()
+            formatted_time = current_time.strftime("%A, %B %d %Y, %H:%M:%S %p")
+            city_weather_update = {
+                'city': city_name,
+                'description': response['weather'][0]['description'],
+                'icon': response['weather'][0]['icon'],
+                'temperature': 'Temperature: '+ str(response['main']['temp']) + '°C',
+                'country_code': response['sys']['country'],
+                'wind': 'Wind: '+ str(response['wind']['speed']) + 'km/h',
+                'humidity': 'Humidity: '+str(response['main']['humidity']) + '%',
+                'time': formatted_time
+                    
+            }
+            
+        else:
+            city_weather_update = {}
+
+        context = {
+            'city_weather_update': city_weather_update
+        }
+            
+        return render(request, 'weatherApp/index.html', context)
+    except:
+       return render(request, 'weatherApp/404.html')
